c311882fd90e92b9a8418fc5d28dd7600385044d
wb_kaojixiang <>
2020-07-31 08:39:04 +0800

    init
    Signed-off-by: wb_kaojixiang <>

---
+++ app/src/main/java/com/example/dagger2demo/Car.java
@@ -0,0 +1,24 @@
+package com.example.dagger2demo;
+
+import android.util.Log;
+
+import javax.inject.Inject;
+
+public class Car {
+
+    private static final String TAG = "Car";
+
+    private Wheels wheels;
+    private Engine engine;
+
+
+    @Inject
+    public Car(Wheels wheels, Engine engine) {
+        this.wheels = wheels;
+        this.engine = engine;
+    }
+    
+    public void drive() {
+        Log.d(TAG, "drive: ");
+    }
+}
--- app/src/main/java/com/example/dagger2demo/MainActivity.java
+++ app/src/main/java/com/example/dagger2demo/MainActivity.java
@@ -10,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CarComponent component = DaggerCarComponent.create();
+
+        Car car = component.getCar();
+
+        car.drive();
     }
 }
